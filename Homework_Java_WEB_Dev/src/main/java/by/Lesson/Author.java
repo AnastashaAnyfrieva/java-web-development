f43b85413f21f2b0b9e16a94a2d22e2115dc9ade
Anastasiya <anastashaanyfrieva@gmail.com>
2024-04-25 22:22:23 +0300

    Добавление произведений одного из авторов

--- Homework_Java_WEB_Dev/src/main/java/by/Lesson/Author.java
+++ Homework_Java_WEB_Dev/src/main/java/by/Lesson/Author.java
@@ -4,5 +4,8 @@ public class Author {
     public static void main(String[] args) {
         System.out.println("Сергей Лукьяненко · Ник Перумов");
         System.out.println("Не время для драконов");
+        System.out.println("Дочь некроманта");
+        System.out.println("Не место для людей");
+
     }
 }
